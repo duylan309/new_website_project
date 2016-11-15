83ee9ffe70e4819ddf7038df19732a71a20d296a
Cuong Bui <phucuong44th1@gmail.com>
2016-11-15 16:55:37 +0700

    structure files

--- app/admin/config/router.php
+++ app/admin/config/router.php
@@ -1,9 +1,9 @@
 <?php
-$router->add('/{query:(/.*)*}', [
+$router->add('/{query:(/.*)*}', array(
     'module'     => 'admin',
     'controller' => 'dashboard',
     'action'     => 'index'
-])->setName('dashboard');
+))->setName('dashboard');
 
 $router->setUriSource(\Phalcon\Mvc\Router::URI_SOURCE_SERVER_REQUEST_URI);
 
