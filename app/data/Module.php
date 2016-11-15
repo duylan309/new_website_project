4171cb9d640944b59310d070b6a3c7cd1532aaed
Cuong Bui <phucuong44th1@gmail.com>
2016-11-15 16:15:59 +0700

    structure files

---
+++ app/data/Module.php
@@ -0,0 +1,22 @@
+<?php
+namespace Thue\Data;
+
+class Module
+{
+    public function registerAutoloaders()
+    {
+        $loader = new \Phalcon\Loader();
+
+        $loader->registerNamespaces([
+            'Thue\Data\Lib'   => ROOT . '/app/data/lib/',
+            'Thue\Data\Model' => ROOT . '/app/data/model/',
+            'Thue\Data\Repo'  => ROOT . '/app/data/repo/'
+        ]);
+        $loader->register();
+    }
+
+    public function registerServices($di)
+    {
+
+    }
+}
