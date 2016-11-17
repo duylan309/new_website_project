d3c4e3e047448f572b2cc533e4ed6c309b1337f4
Cuong Bui <phucuong44th1@gmail.com>
2016-11-17 09:25:40 +0700

    update login/logout

--- app/admin/controller/AuthController.php
+++ app/admin/controller/AuthController.php
@@ -3,5 +3,17 @@ namespace Thue\Admin\Controller;
 
 class AuthController extends BaseController
 {
+    public function loginAction()
+    {
 
+    }
+
+    public function logoutAction()
+    {
+        $this->session->remove('USER');
+        $cookie = $this->cookies->get('USER');
+        $cookie->delete();
+
+        return $this->response->redirect(array('for' => 'auth_login'));
+    }
 }
