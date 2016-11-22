2edc2a745f2529e0574896e1fbca98b278dc8949
Cuong Bui <phucuong44th1@gmail.com>
2016-11-22 11:00:56 +0700

    update grunt file

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,12 +1,13 @@
 module.exports = function (grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+
         cssmin: {
             options: {
                 shorthandCompacting: false,
                 roundingPrecision: -1
             },
-            target: {
+            admin: {
                 files: {
                     'public/admin/asset/css/style.css': [
                         'public/admin/asset/css/bootstrap.min.css',
@@ -16,21 +17,37 @@ module.exports = function (grunt) {
                         'public/admin/asset/css/admin_style.css'
                     ]
                 }
+            },
+            home: {
+                files: {
+                    'public/home/asset/css/style.css': [
+                        'public/admin/asset/css/bootstrap.min.css',
+                        'public/admin/asset/css/fonticon.min.css'
+                    ]
+                }
             }
         },
         concat: {
-            all: {
+            admin: {
                 src: [
                     'public/admin/asset/js/jquery.min.js',
                     'public/admin/asset/js/bootstrap.min.js',
                     'public/admin/asset/js/sortable.min.js'
                 ],
                 dest: 'public/admin/asset/js/script.js'
+            },
+            home: {
+                src: [
+                    'public/home/asset/js/jquery.min.js',
+                    'public/home/asset/js/bootstrap.min.js'
+                ],
+                dest: 'public/home/asset/js/script.js'
             }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-concat');
+
     grunt.registerTask('dev', ['cssmin', 'concat']);
 };
